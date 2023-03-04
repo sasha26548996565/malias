e0d2aee7240b4689f8bdeeb360fc4758c37aa30f
sasha <sashapozhidaev07@example.com>
2023-03-04 16:40:29 +0600

    add cart

--- resources/views/includes/ajax/addCart.blade.php
+++ resources/views/includes/ajax/addCart.blade.php
@@ -6,6 +6,11 @@
                 productId = jQuery(event.target).data('id')
                 quantity = jQuery('.quantityy').val();
 
+            if (quantity == undefined)
+            {
+                quantity = 1;
+            }
+
             jQuery.ajax({
                 url: "{{ route('cart.add') }}",
                 type: "POST",
